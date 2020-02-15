58c92330af00fd4e4c2d14fb56d9a7d18e5c7991
manavnair2000 <55587634+manavnair2000@users.noreply.github.com>
2020-02-15 15:51:33 +0530

    Committed By Manav Notification working perfectly for all android versions

---
+++ app/src/main/java/com/example/madexperiments/ui/experiment_7/ExperimentSevenViewModel.java
@@ -0,0 +1,7 @@
+package com.example.madexperiments.ui.experiment_7;
+
+import androidx.lifecycle.ViewModel;
+
+public class ExperimentSevenViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/madexperiments/ui/experiment_7/experiment_7.java
@@ -0,0 +1,38 @@
+package com.example.madexperiments.ui.experiment_7;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.madexperiments.R;
+
+public class experiment_7 extends Fragment {
+
+    private ExperimentSevenViewModel mViewModel;
+
+    public static experiment_7 newInstance() {
+        return new experiment_7();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.experiment_7, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(ExperimentSevenViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
